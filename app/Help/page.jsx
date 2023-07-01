ab1aa2dba3a82b90f31aa7652bbbad8d8a672f9c
sydvys <aysuseyidova36@gmail.com>
2023-07-01 13:08:20 +0400

    hlp

--- app/Help/page.jsx
+++ app/Help/page.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const Help = () => {
   return (
-    <div className="  text-white bg-black mx-auto ">
+    <div className="  text-white mx-auto ">
       <h1 className="text-[#ff5f53] text-2xl self-start ml-4">Help</h1>
 
-      <div className="grid md:grid-cols-2">
-        <div className="flex flex-col justify-center items-center text-2xl">
+      <div className="grid md:grid-cols-2 w-4/5 mx-auto">
+        <div className="flex flex-col justify-center items-center text-2xl ">
           <h1>Bildilçin - Axtarış imkanları (1-ci hissə)</h1>
-          <video className="bg-white" src=""></video>
+          <video className="bg-white w-96 h-40" src=""></video>
         </div>
-        <div className="flex flex-col justify-center items-center text-2xl">
+        <div className="flex flex-col justify-center items-center text-2xl ">
           <h1>Bildilçin - Axtarış imkanları (2-ci hissə)</h1>
-          <video className="bg-white" src=""></video>
+          <video className="bg-white w-96 h-40" src=""></video>
         </div>
-        <div className="flex flex-col justify-center items-center text-2xl">
+        <div className="flex flex-col justify-center items-center text-2xl ">
           <h1>Bildiçin - Əlavə funksiyalar</h1>
-          <video className="bg-white" src=""></video>
+          <video className="bg-white w-96 h-40" src=""></video>
         </div>
       </div>
 
