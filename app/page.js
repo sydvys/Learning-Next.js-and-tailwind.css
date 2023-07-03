c793f6495902d7917182b3787f5d7a0ff6c8911b
sydvys <aysuseyidova36@gmail.com>
2023-07-03 17:03:50 +0400

    bg color

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Help from "./Help/page";
 export default function Home() {
   return (
     <main className="">
-      <div className="bg-white h-32 flex justify-center items-center mx-auto w-2/5 xl:w-1/2 rounded-md bg-slate-300">
+      <div className="bg-gray-100 h-32 flex justify-center items-center mx-auto w-2/5 xl:w-1/2 rounded-md">
         <form>
           <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
