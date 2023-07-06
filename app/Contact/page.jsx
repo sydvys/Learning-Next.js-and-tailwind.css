a90d05023f56edc8d3f9a2accc77ed5958068800
sydvys <aysuseyidova36@gmail.com>
2023-07-06 18:03:01 +0400

    contact

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -3,95 +3,97 @@
 import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [extra, setExtra] = useState("");
   const [message, setMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = {
+      name,
       email,
-      phone,
-      first: firstName,
-      last: lastName,
+      extra,
       message,
     };
     console.log(data);
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mx-auto w-11/12 ">
-      <form className="container" onSubmit={handleSubmit}>
-        <h1 className="text-red-500">Contact us</h1>
-        <p className="text-white">
+    <div className="flex flex-col justify-center items-center mx-auto w-[88%] ">
+      <form className="container grid gap-5" onSubmit={handleSubmit}>
+        <h1 className="text-[#ff5f53] text-2xl font-extralight">Contact us</h1>
+        <p className="text-white text-sm py-3">
           You can send us your wishes and proposals, or contact us for another
           issue related to the “Bildilçin” project by using the form below.
         </p>
 
-        <div className="py-5 name">
+        <div className="pt-4 name">
           <div>
             <input
-              className="w-full p-3.5 rounded-md"
-              id="frm-first"
+              className="w-full p-3.5 rounded-md bg-[#2e3e70] placeholder:text-gray-500"
+              id="name"
               type="text"
-              name="first"
+              name="name"
               autoComplete="given-name"
               required
-              value={firstName}
+              value={name}
               placeholder="Your name and surname *"
-              onChange={(e) => setFirstName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
         </div>
 
-        <div className="py-5 email">
-          <input
-            className="w-full p-3.5 rounded-md"
-            id="frm-email"
-            type="email"
-            name="email"
-            autoComplete="email"
-            required
-            value={email}
-            placeholder="Your e-mail address *"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+
+        <div className="grid md:grid-cols-2 gap-5">
+          <div className=" email md:grid-col-2 ">
+            <input
+              className="w-full p-3.5 rounded-md bg-[#2e3e70] placeholder:text-gray-500"
+              id="email"
+              type="email"
+              name="email"
+              autoComplete="email"
+              required
+              value={email}
+              placeholder="Your e-mail address *"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className=" email md:grid-col-2 ">
+            <input
+              className="w-full p-3.5 rounded-md bg-[#2e3e70] placeholder:text-gray-500"
+              id="extra"
+              type="text"
+              name="extra"
+              value={extra}
+              placeholder="Institution, which you represent"
+              onChange={(e) => setExtra(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className="py-5 email">
-          <input
-            className="w-full p-3.5 rounded-md"
-            id="frm-email"
-            type="email"
-            name="email"
-            autoComplete="email"
-            required
-            value={email}
-            placeholder="Institution, which you represent"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
 
-        <div className="py-5 message">
-          <textarea
-            className="w-full h-44 rounded-md"
-            id="frm-message"
-            rows="6"
+        <div className=" message">
+          <input
+            className="w-full h-44 rounded-md bg-[#2e3e70] placeholder:absolute placeholder:top-0 placeholder:p-3 placeholder:text-gray-500"
+            id="message"
             name="message"
             value={message}
             placeholder="Your message *"
             onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
+          />
         </div>
 
-        <p className="text-red-500">
+        <p className="text-[#ff5f53] text-xs">
           The mandatory fields are marked with the “ * ” symbol.
         </p>
 
-        <div className="py-5 button flex justify-end" >
-          <button className="bg-red-500 py-2 px-7 rounded-md" type="submit">
+        <div className="py-1 button flex justify-end">
+          <button
+            className="bg-[#ff5f53] py-2 px-8 rounded-md text-white text-sm"
+            type="submit"
+          >
             Send
           </button>
         </div>
