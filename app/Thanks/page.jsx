e2c4446b219aa7eec9f9e1e070c00704583b6991
sydvys <aysuseyidova36@gmail.com>
2023-07-10 16:15:25 +0400

    thanks

--- app/Thanks/page.jsx
+++ app/Thanks/page.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Thanks = () => {
   return (
-    <div>
+    <div className="mx-auto w-[90%]">
       <h1 className="text-[#ff5f53] font-light text-2xl">Thanks</h1>
 
-      <div className=" text-white text-sm ">
+      <div className="text-white text-sm">
         <p className="py-5">
           First, we would like to express our special appreciation to all
-          authors, editors and other compilers of the dictionaries, which were
+          authors, editors, and other compilers of the dictionaries, which were
           used in the application, for their immense and painstaking work.
         </p>
 
@@ -17,21 +17,22 @@ const Thanks = () => {
             Furthermore, we heartily thank everyone who directly participated in
             the project:
           </p>
-          <ul className="list-disc">
+          <ul className="list-disc ml-6">
             <li>
-              The vise-president of Azerbaijan National Academy of Sciences,
-              doctor of philological sciences, professor Habibbayli Isa Akbar
-              oglu for supporting the idea of the project;
+              The vice-president of Azerbaijan National Academy of Sciences,
+              doctor of philological sciences, professor{" "}
+              <span className="text-[#ff5f53]">Habibbayli Isa Akbar oglu</span> for supporting the idea of
+              the project;
             </li>
             <li>
-              The Director of Institute of Linguistics named after Nasimi of
+              The Director of the Institute of Linguistics named after Nasimi of
               Azerbaijan National Academy of Sciences, doctor of philological
-              sciences, professor Naghisoylu Mohsun Zellabdin oglu for providing
-              an opportunity to use the dictionaries compiled by Institute;
+              sciences, professor <span className="text-[#ff5f53]">Naghisoylu Mohsun Zellabdin oglu</span> for providing
+              an opportunity to use the dictionaries compiled by the Institute;
             </li>
             <li>
-              The students of Azerbaijan University of Languages – Samira
-              Aliyeva Rauf and Javidan Javanshirli Fakhri, for their voluntary
+              The students of Azerbaijan University of Languages – <span className="text-[#ff5f53]">Samira
+              Aliyeva Rauf</span> and <span className="text-[#ff5f53]">Javidan Javanshirli Fakhri</span>, for their voluntary
               and dedicated work on preparing e-versions of the dictionaries
               used in the application.
             </li>
